a7d072ad85f301a1c3a0a882ddfb612010a1b49c
Danik <retikadni@gmail.com>
2024-09-11 15:15:55 +0200

    Better test

--- tests/auth.py
+++ tests/auth.py
@@ -1,40 +1,113 @@
-import requests
+import time
+import aiohttp
+import asyncio
+import random
+import string
 
-username = input("Username> ")
-email = input("Email> ")
-password = input("Password> ")
 
-response = requests.post(
-    "http://localhost:6169/v1/auth/register",
-    json={
-        "username": username,
-        "email": email,
-        "password": password
-    }
-)
+def generate_random_string(length: int) -> str:
+    return ''.join(random.choices(string.ascii_lowercase, k=length))
 
-print(response.text)
 
-email = input("Email> ")
-password = input("Password> ")
+async def register(username: str, email: str, password: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            "http://localhost:6169/v1/auth/register",
+            json={
+                "username": username,
+                "email": email,
+                "password": password
+            }
+        ) as response:
+            response.raise_for_status()
+            return await response.text()
 
-response = requests.post(
-    "http://localhost:6169/v1/auth/login",
-    json={
-        "email": email,
-        "password": password
-    }
-)
 
-print(response.text)
+async def login(email: str, password: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            "http://localhost:6169/v1/auth/login",
+            json={
+                "email": email,
+                "password": password
+            }
+        ) as response:
+            response.raise_for_status()
+            return await response.text()
 
-token = input("Refresh> ")
 
-response = requests.post(
-    "http://localhost:6169/v1/auth/refresh",
-    json={
-        "refresh_token": token
-    }
-)
+async def refresh(token: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            "http://localhost:6169/v1/auth/refresh",
+            json={
+                "refresh_token": token
+            }
+        ) as response:
+            response.raise_for_status()
+            return await response.text()
 
-print(response.text)
+
+async def stress() -> None:
+    n = int(input("Number of accounts> "))
+    print(":: Generating account list")
+    accounts = []
+    for _ in range(n):
+        nickname = generate_random_string(10)
+        mail = generate_random_string(12) + "@gmail.com"
+        password = generate_random_string(32)
+        accounts.append((nickname, mail, password))
+
+    print(":: Registering accounts...")
+    start = time.perf_counter()
+
+    tasks = []
+    for x in accounts:
+        tasks.append(asyncio.create_task(register(x[0], x[1], x[2])))
+    await asyncio.gather(*tasks)
+
+    _time = round(time.perf_counter()-start, 5)
+    print(f":: Success! (Time {_time})")
+
+    print(":: Logging into accounts")
+    start = time.perf_counter()
+
+    tasks = []
+    for x in accounts:
+        tasks.append(asyncio.create_task(login(x[1], x[2])))
+    await asyncio.gather(*tasks)
+    _time = round(time.perf_counter()-start, 5)
+    print(f":: Success! (Time {_time})")
+
+
+async def main() -> None:
+    print(
+        "1. Register",
+        "2. Login",
+        "3. Refresh",
+        "4. Stress test",
+        sep="\n"
+    )
+    choice = input("Select test> ")
+    match choice:
+        case "1":
+            username = input("Username> ")
+            email = input("Email> ")
+            password = input("Password> ")
+
+            print(await register(username, email, password))
+        case "2":
+            email = input("Email> ")
+            password = input("Password> ")
+
+            print(await login(email, password))
+        case "3":
+            token = input("Refresh> ")
+
+            print(await refresh(token))
+        case "4":
+            await stress()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
