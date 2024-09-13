d670c370f98256b0dd8cf0f0b2560ee680f437fb
Danik <retikadni@gmail.com>
2024-09-13 23:09:48 +0200

    Fix

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if ! systemctl is-active --quiet postgresql; then
     sudo systemctl start postgresql
 fi
 
-hypercorn -w 8 -b 0.0.0.0:6169 api:app
+hypercorn -w 8 -b 0.0.0.0:6169 api:app --keep-alive 30
