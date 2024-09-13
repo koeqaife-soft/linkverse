2eb3557707a4f1332fc39d56a46daa3acbb8b65a
Danik <retikadni@gmail.com>
2024-09-13 23:04:59 +0200

    Use Hypercorn instead of gunicorn+uvicorn

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if ! systemctl is-active --quiet postgresql; then
     sudo systemctl start postgresql
 fi
 
-gunicorn -w 8 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:6169 api:app
+hypercorn -w 8 -b 0.0.0.0:6169 api:app
