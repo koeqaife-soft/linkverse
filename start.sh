6cedf23f1df8bd185923a605c08f65648ccbfe40
Danik <retikadni@gmail.com>
2024-09-14 16:06:19 +0200

    Better script

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+source .env
+sleep 1
+
 if ! systemctl is-active --quiet postgresql; then
     sudo systemctl start postgresql
 fi
 
-hypercorn -w 8 -b 0.0.0.0:6169 api:app --keep-alive 30
+if [[ "$DEBUG" == "True" ]]; then
+    WORKER_COUNT=2
+else
+    WORKER_COUNT=$(nproc)
+fi
+
+hypercorn -w "$WORKER_COUNT" -b 0.0.0.0:6169 api:app --keep-alive 30 --log-level debug
