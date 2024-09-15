1de7191421a60fe53e3d91e78b838d8f6e30e6b3
Danik <retikadni@gmail.com>
2024-09-15 19:37:44 +0200

    Worker count argument & env variable

--- start.sh
+++ start.sh
@@ -2,18 +2,24 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 source .env
-sleep 1
 
 if ! systemctl is-active --quiet postgresql; then
     sudo systemctl start postgresql
 fi
 
-if [[ "$DEBUG" == "True" ]]; then
+if [[ $# -gt 0 && $1 =~ ^[0-9]+$ ]]; then
+    WORKER_COUNT=$1
+elif [[ "$DEBUG" == "True" ]]; then
     WORKER_COUNT=2
 else
     WORKER_COUNT=$(nproc)
 fi
 
+export _WORKER_COUNT=$WORKER_COUNT
+export PYTHONPATH=$SCRIPT_DIR
+
 python -OO init_db.py
-hypercorn -w "$WORKER_COUNT" -b 0.0.0.0:6169 api:app --keep-alive 30 --log-level debug
+hypercorn -w "$WORKER_COUNT" -b 0.0.0.0:6169 api:app --keep-alive 30 --log-level critical
