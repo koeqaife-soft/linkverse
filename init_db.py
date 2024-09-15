e360de5d9fad403b9ba6ba1bc8786a45c370c1c5
Danik <retikadni@gmail.com>
2024-09-15 19:39:04 +0200

    Use logger

--- init_db.py
+++ init_db.py
@@ -1,17 +1,20 @@
 from utils.database import initialize_database
+from core import setup_logger
 import asyncio
 import json
 import asyncpg
 
+logger = setup_logger()
+
 
 async def main():
-    print(":: Running PostgreSQL init scripts")
+    logger.info("Running PostgreSQL init scripts")
     with open("postgres.json") as f:
         config = json.load(f)
     conn = await asyncpg.connect(**config)
     await initialize_database(conn)
     await conn.close()
-    print(":: Done!")
+    logger.info("Done!")
 
 
 if __name__ == "__main__":
