f8e35f86c274d386e07b93421eb8e7c92e215490
Danik <retikadni@gmail.com>
2024-09-28 17:37:06 +0200

    Posts

---
+++ extensions/posts.py
@@ -0,0 +1,81 @@
+import asyncpg
+from quart import Blueprint, Quart
+from core import response, Global, route, error_response
+from quart import g
+import utils.posts as posts
+
+bp = Blueprint('posts', __name__)
+_g = Global()
+pool: asyncpg.Pool = _g.pool
+
+
+@route(bp, "/posts", methods=["POST"])
+async def create_post() -> tuple[str, int]:
+    data = g.data
+    content = data.get('content')
+
+    async with pool.acquire() as db:
+        result = await posts.create_post(g.user_id, content, db)
+
+    if not result.success:
+        return error_response(result), 500
+
+    return response(data=result.data or {}), 201
+
+
+@route(bp, "/posts/<int:id>", methods=["GET"])
+async def get_post(id: int):
+    async with pool.acquire() as db:
+        result = await posts.get_post({"post_id": id}, db)
+
+    if not result.success:
+        return error_response(result), 400
+
+    if result.data is None:
+        return response(error=True, error_msg="UNKNOWN_ERROR")
+
+    return response(data=result.data.to_dict()), 200
+
+
+@route(bp, "/posts/<int:id>", methods=["DELETE"])
+async def delete_post(id: int):
+    async with pool.acquire() as db:
+        post = await posts.get_post({"post_id": id}, db)
+        if not post.success:
+            return error_response(post), 400
+        if post.data.user_id != g.user_id:  # type: ignore
+            return response(error=True, error_msg="FORBIDDEN"), 403
+        result = await posts.delete_post(id, db)
+
+    if not result.success:
+        return error_response(result), 500
+
+    return response()
+
+
+@route(bp, "/posts/<int:id>", methods=["PATCH"])
+async def update_post(id: int):
+    data = g.data
+    content: str | None = data.get("content")
+    tags: list[str] | None = data.get("tags")
+    media: list[str] | None = data.get("media")
+
+    if content is None and tags is None and media is None:
+        return response(error=True, error_msg="INCORRECT_DATA"), 400
+
+    async with pool.acquire() as db:
+        post = await posts.get_post({"post_id": id}, db)
+        if not post.success:
+            return error_response(post), 400
+        if post.data.user_id != g.user_id:  # type: ignore
+            return response(error=True, error_msg="FORBIDDEN"), 403
+        result = await posts.update_post(id, content, tags, media, db)
+
+    if not result.success:
+        return error_response(result), 500
+
+    return response()
+
+
+def load(app: Quart):
+    app.register_blueprint(bp)
