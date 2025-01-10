fea4944d27d0457c1498a71c1a1ff0cd88b88e5a
Danik <retikadni@gmail.com>
2025-01-10 21:18:43 +0100

    Favorite list

--- utils/users.py
+++ utils/users.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, asdict
 from utils.generation import parse_id
 from core import Status, FunctionError
 from utils.database import AutoConnection
+import typing as t
 
 
 @dataclass
@@ -159,15 +160,14 @@ async def add_to_favorites(
     post_id: str | None = None,
     comment_id: str | None = None
 ) -> Status[None]:
-    key = "comment_id" if comment_id else "post_id"
     db = await conn.create_conn()
     async with db.transaction():
         await db.execute(
-            f"""
-                INSERT INTO favorites (user_id, {key})
-                VALUES ($1, $2)
-                ON CONFLICT ({key}, user_id) DO NOTHING
-            """, user_id, comment_id or post_id
+            """
+                INSERT INTO favorites (user_id, comment_id, post_id)
+                VALUES ($1, $2, $3)
+                ON CONFLICT (post_id, comment_id, user_id) DO NOTHING
+            """, user_id, comment_id, post_id
         )
 
     return Status(True)
@@ -208,3 +208,53 @@ async def is_favorite(
     )
 
     return Status(True, data=row is not None)
+
+
+async def get_favorites(
+    user_id: str, conn: AutoConnection,
+    cursor: str | None = None,
+    type: t.Literal["posts", "comments"] | None = None
+) -> Status[list[str]]:
+    db = await conn.create_conn()
+    query = """
+        SELECT post_id, comment_id, created_at
+        FROM favorites WHERE user_id = $1
+    """
+    params = [user_id]
+
+    if cursor:
+        query += " AND created_at < $2"
+        params.append(cursor)
+
+    if type == "posts":
+        query += " AND post_id IS NOT NULL AND comment_id IS NULL"
+    elif type == "comments":
+        query += " AND comment_id IS NOT NULL"
+
+    query += " ORDER BY created_at DESC LIMIT 21"
+
+    rows = await db.fetch(query, *params)
+    if not rows:
+        raise FunctionError("NO_MORE_FAVORITES", 200, None)
+
+    favorites = [
+        {
+            "post_id": row["post_id"],
+            "comment_id": row["comment_id"],
+            "created_at": row["created_at"].isoformat(),
+        }
+        for row in rows
+    ]
+
+    has_more = len(rows) > 20
+    rows = rows[:20]
+
+    last_row = rows[-1]
+
+    next_cursor = last_row["created_at"].isoformat() if rows else None
+
+    return Status(
+        True,
+        data={"favorites": favorites, "next_cursor": next_cursor,
+              "has_more": has_more}
+    )
