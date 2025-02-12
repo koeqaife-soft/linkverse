7e3dd76c53d54286ff555da4347c815ed9534a03
Danik <retikadni@gmail.com>
2025-02-12 16:19:43 +0100

    Types file
    Named "schemas" because I can't use "types"

---
+++ schemas.py
@@ -0,0 +1,36 @@
+from typing import TypedDict
+
+
+class ListsDefault(TypedDict):
+    has_more: bool
+    next_cursor: str | None
+
+
+class FollowedItem(TypedDict):
+    created_at: str
+    followed_to: str
+
+
+class FollowedList(TypedDict, ListsDefault):
+    followed: list[FollowedItem]
+
+
+class FavoriteItem(TypedDict):
+    post_id: str
+    comment_id: str
+    created_at: str
+
+
+class FavoriteList(TypedDict, ListsDefault):
+    favorites: list[FavoriteItem]
+
+
+class ReactionItem(TypedDict):
+    post_id: str
+    comment_id: str
+    created_at: str
+    is_like: bool
+
+
+class ReactionList(TypedDict, ListsDefault):
+    reactions: list[ReactionItem]
