3dbd19a00de0c0b7edcb4ef8ce48113221d90c5b
Danik <retikadni@gmail.com>
2025-10-28 13:28:36 +0100

    Secret key number

--- utils/generation.py
+++ utils/generation.py
@@ -8,7 +8,7 @@ from utils_cy.encryption import verify_signature, generate_signature
 from utils_cy.snowflake import SnowflakeGeneration
 
 
-SECRET_KEY = os.environ["SIGNATURE_KEY"].encode()
+SIGNATURE_KEY = os.environ["SIGNATURE_KEY"].encode()
 
 
 async def generate_token(
@@ -17,9 +17,10 @@ async def generate_token(
     secret: str = "12",
     session_id: str = "abc"
 ) -> str:
+    key_num, key = key.split(":", 1)
     expiration = int((
         datetime.datetime.now(datetime.UTC) +
-        (datetime.timedelta(hours=12) if not long_term
+        (datetime.timedelta(hours=1) if not long_term
          else datetime.timedelta(days=30))
     ).timestamp())
 
@@ -27,13 +28,14 @@ async def generate_token(
     nonce = generate_nonce(8)
     encrypted_data = encode(combined_data, key + nonce)
 
-    signature = generate_signature(encrypted_data, SECRET_KEY)
+    signature = generate_signature(encrypted_data, SIGNATURE_KEY)
 
-    token = f"{nonce}{encrypted_data}.{signature}"
+    token = f"{key_num}:{nonce}{encrypted_data}.{signature}"
     return f"LV {token}"
 
 
 async def decode_token(token: str, key: str) -> dict:
+    key_num, key = key.split(":", 1)
     try:
         if not token.startswith("LV "):
             return {
@@ -42,6 +44,18 @@ async def decode_token(token: str, key: str) -> dict:
             }
 
         token = token[3:]
+
+        if token.count(':') != 1:
+            token_key_num = "1"
+        else:
+            token_key_num, token = token.split(':', 1)
+
+        if token_key_num != key_num:
+            return {
+                "success": False,
+                "msg": "EXPIRED_TOKEN"
+            }
+
         nonce, token = token[:16], token[16:]
         token_parts = token.rsplit('.', 1)
         if len(token_parts) != 2:
@@ -52,7 +66,7 @@ async def decode_token(token: str, key: str) -> dict:
 
         token_payload, signature = token_parts
 
-        if not verify_signature(token_payload, signature, SECRET_KEY):
+        if not verify_signature(token_payload, signature, SIGNATURE_KEY):
             return {
                 "success": False,
                 "msg": "INVALID_SIGNATURE"
